e209a42e228338fd73c81adea035a98988e6096b
Yochay Cohen-Or <yochai.cohenor@gmail.com>
2025-03-05 13:02:55 +0200

    Some basic content for the main page

--- src/layouts/MainPage/MainPage.tsx
+++ src/layouts/MainPage/MainPage.tsx
@@ -1,11 +1,27 @@
-// import './MainPage.css'
+import './MainPage.css'
 
 function MainPage() {
+  const sendToAnalyze = () => {
+    console.log('Analyzing...')
+  }
 
   return (
-    <>
+    <div className='main-page'>
       <h1>GovAIna - Government AI decisioN Analyzer</h1>
-    </>
+      <div className="inputs-form">
+        <label>Decision Number</label>
+        <input className='decision-number-input' type='number' />
+        <label>Decision text</label>
+        <input className='decision-text-input' />
+        <button className='analyze-button' onClick={sendToAnalyze}>Analyze</button>
+      </div>
+      <div className='analyzation-section'>
+        <div className='analyzation-result'>
+          <h2>Result</h2>
+          <p>Decision text: Lorem ipsum dolor sit amet, consectetur adipiscing elit Lorem ipsum dolor sit amet, consectetur adipiscing elit Lorem ipsum dolor sit amet, consectetur adipiscing elit Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
