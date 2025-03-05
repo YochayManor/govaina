f0030d9fad8ad8d4117cdbd348d8ec9c52b5e534
Yochay Cohen-Or <yochai.cohenor@gmail.com>
2025-03-05 16:02:59 +0200

    Add error catcher - a simple one

--- src/api/db/analyzations.ts
+++ src/api/db/analyzations.ts
@@ -1,15 +1,21 @@
+import { catchError } from "../common/errorHandler"
+
 const DB_HOST = 'http://localhost:9000'
 
-export const checkForExistingAnalyzation = async (decisionNumber: number): Promise<[Error] | [null, string | null]> => {
-    const response = await fetch(`${DB_HOST}/analyzations/${decisionNumber}`, {
+export const checkForExistingAnalyzation = async (decisionNumber: number): Promise<[Error] | [undefined, string | null]> => {
+    const [error, response] = await catchError(fetch(`${DB_HOST}/analyzations/${decisionNumber}`, {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json'
         }
-    })
+    }))
+
+    if (error) {
+        return [error] // TODO: handle error
+    }
 
     if (response.status === 200) {
-        return [null, await response.text() || null]
+        return [undefined, await response.text() || null]
     }
     else {
         return [new Error('Something wrong happened when checking for existing analyzation')]
--- src/api/openai/assistant.ts
+++ src/api/openai/assistant.ts
@@ -1,16 +1,22 @@
+import { catchError } from "../common/errorHandler"
+
 const OPENAI_HOST = 'http://localhost:8000/analyze'
 
-export const analyze = async (decisionNumber: number, decisionText: string): Promise<[Error] | [null, string]> => {
-  const response = await fetch(OPENAI_HOST, {
+export const analyze = async (decisionNumber: number, decisionText: string): Promise<[Error] | [undefined, string]> => {
+  const [error, response] = await catchError(fetch(OPENAI_HOST, {
     method: 'POST',
     body: JSON.stringify({ decisionNumber, decisionText }),
     headers: {
       'Content-Type': 'application/json'
     }
-  })
+  }))
+
+  if (error) {
+    return [error] // TODO: handle error
+  }
 
   if (response.status === 200) {
-    return [null, await response.text()]
+    return [undefined, await response.text()]
   }
   else {
     return [new Error('Failed to analyze decision')]
